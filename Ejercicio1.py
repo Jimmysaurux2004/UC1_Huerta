3ad08e28bd2c9138cdbd34a6988c3e537f8ac563
Jimmy Huerta Vasquez <2123010236@untels.edu.pe>
2024-04-19 13:47:46 -0500

    Todos los archivos actualizados

--- Ejercicio1.py
+++ Ejercicio1.py
@@ -1,1 +1,14 @@
 #EJERCICIO1
+'''
+Cálculo del promedio ponderado: Solicita al usuario que ingrese las notas y los pesos de varios elementos 
+(por ejemplo, exámenes, tareas) y calcula el promedio ponderado.
+'''
+def promedio_ponderado(notas, pesos):
+    suma = sum(nota * peso for nota, peso in zip(notas, pesos))
+    total_pesos = sum(pesos)
+    return suma / total_pesos if total_pesos != 0 else 0
+
+# Ejemplo de uso
+notas = [float(input("Ingrese la nota {}: ".format(i + 1))) for i in range(3)]
+pesos = [float(input("Ingrese el peso para la nota {}: ".format(i + 1))) for i in range(3)]
+print("El promedio ponderado es:", promedio_ponderado(notas, pesos))
--- Ejercicio4.py
+++ Ejercicio4.py
@@ -1,1 +1,12 @@
 #EJERCICIO4
+'''
+Cálculo del área de un círculo: Solicita al usuario que ingrese el radio de un círculo y calcula su área.
+'''
+import math
+
+def area_circulo(radio):
+    return math.pi * radio ** 2
+
+# Ejemplo de uso
+radio = float(input("Ingrese el radio del círculo: "))
+print("El área del círculo es:", area_circulo(radio))
