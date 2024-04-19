71f61859995809c915b480fe89b4ea98331930ea
Jimmy Huerta Vasquez <2123010236@untels.edu.pe>
2024-04-19 17:41:48 -0500

    v':

--- Ejercicio3.py
+++ Ejercicio3.py
@@ -1,11 +1,1 @@
-#EJERCICIO3
-'''
-Cálculo de la velocidad media: Solicita al usuario que ingrese la distancia recorrida y el tiempo transcurrido, y calcula la velocidad media.
-'''
-def velocidad_media(distancia, tiempo):
-    return distancia / tiempo
-
-# Ejemplo de uso
-distancia = float(input("Ingrese la distancia recorrida (en metros): "))
-tiempo = float(input("Ingrese el tiempo transcurrido (en segundos): "))
-print("La velocidad media es:", velocidad_media(distancia, tiempo), "m/s")
+#3
